9c7c441b5434b79f1fdf847691b6aad5625cbf57
Anjola Adeyemi <anjolaadeyemi4545@gmail.com>
2024-10-30 17:53:41 +0100

    TaikoHelka

--- script/DeployTaikoLend.s.sol
+++ script/DeployTaikoLend.s.sol
@@ -5,9 +5,17 @@ import "forge-std/Script.sol";
 import "../src/TaikoLend.sol";
 
 contract DeployTaikoLend is Script {
+    event TaikoLendDeployed(address indexed contractAddress);
+
     function run() external {
         vm.startBroadcast();
-        new TaikoLend(0xDCe5d4DF2A6708e1b8673eeB5e30DB2bC0aC82Df);
+        
+        // Deploying the TaikoLend contract
+        address taikoLendAddress = address(new TaikoLend(0xDCe5d4DF2A6708e1b8673eeB5e30DB2bC0aC82Df));
+        
+        // Emitting event with the deployed contract address
+        emit TaikoLendDeployed(taikoLendAddress);
+        
         vm.stopBroadcast();
     }
 }
